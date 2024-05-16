1c4660fab39e82bae85243c97f100eb1c7e968d2
mikegag <Michael.gagliardi1@hotmail.com>
2024-05-16 12:33:53 -0400

    created landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Landing from "./pages/Landing"
 
 export default function App() {
   return (
-    <div className=" bg-main-color-lightgreen">
-      <h1>SkillJa </h1>
+    <div className="p-2">
+      <h1>SkillJa </h1> 
     </div>
   )
 }
 
+ 
