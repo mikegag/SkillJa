b22dc852baac01c4b0a6b3584a915ba2e984af54
mikegag <Michael.gagliardi1@hotmail.com>
2024-05-30 10:48:48 -0400

    updated colour scheme, updated landing page with new colour scheme, created CoachIntroduction and AthleteIntroduction pages

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,39 +1,47 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
 import { Link } from "react-router-dom"
 
+
 export default function Landing(){
     return (
-        <div className="h-dvh bg-main-color-white flex flex-col justify-center items-center">
-            <h1 className="text-center text-main-color-darkgreen font-medium text-4xl px-4 font-source">
+        <div className="h-dvh bg-main-color-white flex flex-col justify-center items-center p-2">
+            <h1 className="text-center text-main-green-700 font-medium text-4xl px-4 font-source">
                 Athletic Performance Unleashed
-                <span className="text-main-color-gold ml-1">.</span>
+                <span className="text-main-green-200 ml-1">.</span>
             </h1>
             <img 
-                src={require('../assets/dudes-ready-to-race.png')} 
-                className="w-72 pl-2 mt-6 mb-5"
-                alt="two men kneeling down ready to race each other"
+                src={require('../assets/main-landing-racers.png')} 
+                className="w-72 mt-6 mb-5 lg:w-80"
+                alt="two runners kneeling down ready to race each other"
             />
-            <form className="mx-auto">   
-                <input 
-                    type="search" 
-                    id="default-search" 
-                    className="rounded-3xl py-3 px-12 bg-white text-slate-500 font-kulim border-main-color-darkgreen border-solid border" 
-                    placeholder="Find coaches near me..."
-                    size={21} 
-                />
+            <form className="mx-auto w-72">   
+                <div className="relative w-max">
+                    <input
+                        type="search"
+                        id="default-search"
+                        className="rounded-2xl w-72 py-3 px-10 pl-12 bg-white text-main-grey-300 font-kulim border-main-grey-400 border-solid border"
+                        placeholder="Find coaches near me..."
+                    />
+                    <FontAwesomeIcon
+                        icon={faMagnifyingGlass}
+                        className="absolute inset-y-4 left-0 flex items-center pl-4 text-main-grey-500"
+                    />
+                </div>
             </form>
             <Link to={'/signup'} className="mt-5 mb-9">
                 <button 
-                    className="bg-main-color-darkgreen text-lg font-medium text-main-color-white font-kulim px-16 py-3 rounded-3xl"
+                    className="bg-main-green-500 text-lg font-semibold text-main-cream font-kulim px-16 py-3 rounded-2xl w-72"
                     aria-label="create an account button that redirects to sign up page"
                     >
                         Create an Account
                 </button>
             </Link>
-            <p className="text-gray-400 font-source">
+            <p className="text-main-grey-300 font-source">
                 Already have an account? 
                 <Link to={'/login'}>
-                    <span className="underline text-main-color-darkgreen ml-2">Login Here</span>
+                    <span className="underline text-main-green-700 ml-2">Login Here</span>
                 </Link>
             </p>
         </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,6 +11,25 @@ module.exports = {
           'main-color-gold': '#ffba00',
           'main-color-white': '#fffbf2',
           'main-color-beige': '#f1ecd5',
+
+
+          'main-green-100':'#C9EBCD',
+          'main-green-200':'#B1D5B6',
+          'main-green-300':'#9AC09F',
+          'main-green-400':'#83AB89',
+          'main-green-500':'#6D9773',
+          'main-green-600':'#507656',
+          'main-green-700':'#35563A',
+
+          'main-grey-100':'#E3E3E3',
+          'main-grey-200':'#A3A3A3',
+          'main-grey-300':'#8A8A8A',
+          'main-grey-400':'#5C5C5C',
+          'main-grey-500':'#474747',
+
+          'main-white':'#FFFFFF',
+          'main-cream':'#FFFBF2',
+          'main-black':'#000000'
         },
         fontFamily: {
           kulim: ['Kulim Park'],
