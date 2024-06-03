cfc29ec7c5d46ed8c8fcc0910bc624015080b1f5
mikegag <Michael.gagliardi1@hotmail.com>
2024-06-02 20:59:04 -0400

    updated styling in login and sign up pages

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -18,7 +18,7 @@ export default function Login(){
 
     return (
         <div className="flex flex-col h-dvh p-2">
-            <h2 className="heading mt-24">Welcome Back!</h2>
+            <h2 className="heading mt-20">Welcome Back!</h2>
             <div className="flex flex-col justify-center items-center py-12">
                 <form className="flex flex-col justify-center w-full mx-auto px-4 md:w-7/12 lg:w-4/12" onSubmit={handleSubmit}>
                     <div className="relative w-full">
@@ -48,6 +48,7 @@ export default function Login(){
                     <button
                         className="w-full form-btn mx-auto"
                         type="submit"
+                        aria-label="login form submission"
                     >
                         Login
                     </button>
@@ -57,13 +58,13 @@ export default function Login(){
                     <p className="mx-3 text-main-grey-200">Or Login with</p>
                     <div className="bg-main-grey-300 h-0.5 w-28 lg:w-40"></div>
                 </div>
-                <div className="mb-16">
+                <div className="mb-9">
                     <SignInPartners />
                 </div>
                 <p className="text-main-grey-300 font-kulim">
                     Don't have an account? 
                     <Link to={'/signup'}>
-                        <span className="underline cursor-pointer text-main-color-darkgreen ml-1">
+                        <span className="underline cursor-pointer text-main-green-700 ml-1 hover:text-main-green-500">
                             Sign Up Here
                         </span>
                     </Link>
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react"
 import SignInPartners from "../components/SignInPartners"
 import { Link, useNavigate } from "react-router-dom"
+import { faEnvelope, faUser } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLock } from "@fortawesome/free-solid-svg-icons"
+import data from "../data.json"
 
 export default function SignUp(){
     useEffect(() => {
@@ -14,47 +18,78 @@ export default function SignUp(){
     }
 
     return (
-        <div className="flex flex-col justify-end h-dvh bg-main-color-darkgreen">
-            <div className="flex flex-col justify-center items-center bg-main-color-white rounded-t-3xl py-14 lg:py-10">
-                <div className="mt-3 mb-14">
-                    <SignInPartners />
-                </div>
+        <div className="flex flex-col h-dvh p-2">
+            <h2 className="heading mt-20">{data.signup[0].title}</h2>
+            <div className="flex flex-col justify-center items-center py-12">
                 <form className="flex flex-col justify-center w-full mx-auto px-11 md:w-7/12 lg:w-4/12" onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        className="form-input mb-9"
-                        placeholder="Full Name"
-                        required
-                    />
-                    <input
-                        type="email"
-                        className="form-input mb-9"
-                        placeholder="Email"
-                        required
-                    />
-                    <input
-                        type="password"
-                        className="form-input mb-9"
-                        placeholder="Password"
-                        required
-                    />
-                    <input
-                        type="password"
-                        className="form-input mb-5"
-                        placeholder="Confirm Password"
-                        required
-                    />
+                    <div className="relative w-full">
+                        <input
+                            type="text"
+                            className="form-input mb-5"
+                            placeholder="Full Name"
+                            required
+                        />
+                         <FontAwesomeIcon
+                            icon={faUser}
+                            className="absolute inset-y-4 left-0 flex items-center pl-4 text-main-grey-500"
+                        />
+                    </div>
+                    <div className="relative w-full">
+                        <input
+                            type="email"
+                            className="form-input mb-5"
+                            placeholder="Email"
+                            required
+                        />
+                        <FontAwesomeIcon
+                            icon={faEnvelope}
+                            className="absolute inset-y-4 left-0 flex items-center pl-4 text-main-grey-500"
+                        />
+                    </div>
+                    <div className="relative w-full">
+                        <input
+                            type="password"
+                            className="form-input mb-5"
+                            placeholder="Password"
+                            required
+                        />
+                        <FontAwesomeIcon
+                            icon={faLock}
+                            className="absolute inset-y-4 left-0 flex items-center pl-4 text-main-grey-500"
+                        />
+                    </div>
+                    <div className="relative w-full">
+                        <input
+                            type="password"
+                            className="form-input mb-5"
+                            placeholder="Confirm Password"
+                            required
+                        />
+                        <FontAwesomeIcon
+                            icon={faLock}
+                            className="absolute inset-y-4 left-0 flex items-center pl-4 text-main-grey-500"
+                        />
+                    </div>
                     <button
-                        className="form-btn"
+                        className="w-full form-btn mx-auto"
+                        aria-label="sign up form submission"
                         type="submit"
                     >
                         Next Steps
                     </button>
                 </form>
-                <p className="text-gray-400 font-source">
+                <div className="flex justify-center items-center my-7">
+                    <div className="bg-main-grey-300 h-0.5 w-28 lg:w-40"></div>
+                    <p className="mx-3 text-main-grey-200">Or Sign Up With</p>
+                    <div className="bg-main-grey-300 h-0.5 w-28 lg:w-40"></div>
+                </div>
+                <div className="mb-9">
+                    <SignInPartners />
+                </div>
+                <p className="text-main-grey-300 font-kulim">
                     Already have an account? 
                     <Link to={'/login'}>
-                        <span className="underline cursor-pointer text-main-color-darkgreen ml-1">
+                        <span className="underline cursor-pointer text-main-green-700 ml-1 hover:text-main-green-500">
                             Login Here
                         </span>
                     </Link>
