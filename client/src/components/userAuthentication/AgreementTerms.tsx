6a3e2334369efe396148c3763989d7fa9e7b848d
mikegag <Michael.gagliardi1@hotmail.com>
2024-06-13 12:28:58 -0400

    updated data file with new sports, privacy policy, and terms/agreements. ALso updated signup page to use refactored AgreementTerms component

--- client/src/components/userAuthentication/AgreementTerms.tsx
+++ client/src/components/userAuthentication/AgreementTerms.tsx
@@ -1,5 +1,7 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import data from "../../data.json"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faX } from "@fortawesome/free-solid-svg-icons"
 
 interface formProps {
     isClicked: boolean
@@ -8,53 +10,76 @@ export default function AgreementTerms({isClicked}:formProps){
     const [formToDisplay, setFormToDisplay] = useState<string>("")
     const [clicked, setClicked] = useState<boolean>(false)
 
+    useEffect(()=>{
+        setClicked(clicked)
+    },[clicked])
+
     function viewAgreement(input:string):void{
         setFormToDisplay(input)
-    }
-
-    function handleSubmit(e:React.FormEvent<HTMLFormElement>){
-        e.preventDefault()
-        setClicked(!clicked)
-        isClicked = clicked
     }
 
     return (
         <div>
             {
-                formToDisplay === "terms"?
-                    <div className="w-dvw h-dvh flex justify-center items-center bg-black bg-opacity-20">
-                        <div className="bg-main-color-white rounded-2xl border border-main-black w-64 h-80 overflow-scroll p-4">
-                            {data.terms.information.map((info,index)=>(
-                                <>
-                                    <h3 key={index}>{info.title}</h3>
-                                    <p key={index}>{info.body}</p>
-                                </>
-                            ))}
-                        </div>
+            formToDisplay === "terms"?
+                <div className="w-dvw h-dvh flex justify-center items-center bg-black bg-opacity-60 absolute top-0 left-0">
+                    <div className="flex flex-col bg-white rounded-2xl border border-main-black w-80 h-96 overflow-scroll p-4">
+                        <button className="ml-auto mr-0 hover:text-main-green-500" onClick={()=>setFormToDisplay("")}>
+                            <FontAwesomeIcon icon={faX}/>
+                        </button>
+                        {data.terms.terms_conditions.map((info,index)=>(
+                            <>
+                                    <h3 key={index + "title"} className={`font-bold w-full ${index===0? "text-center underline mb-2 text-lg":"mb-2"}`}>
+                                    { info.title}
+                                </h3>
+                                <p key={index + "body"} className="mb-6">
+                                    {info.body}
+                                </p>
+                            </>
+                        ))}
                     </div>
-                :
-                (
-                    formToDisplay === "privacy"? ""
-                    :
-                    <form onSubmit={handleSubmit} className="mb-5 text-center">
-                        <input
-                            type={data.terms.inputs[0].type}
-                            name="agreement"
-                        />
-                        <label htmlFor="agreement" className="ml-4 text-center">
-                            I have read and agree to the 
-                            <span className="cursor-pointer text-main-green-500 underline ml-2" onClick={()=>viewAgreement("terms")}>
-                                Terms and Conditions
-                            </span>
-                            , and 
-                            <span className="cursor-pointer text-main-green-500 underline ml-2" onClick={()=>viewAgreement("privacy")}>
-                                Privacy Policy.
-                            </span> 
-                        </label>
-                    </form>
-                )
-                
-            }
-        </div>
+                </div>
+            :
+            (
+            formToDisplay === "privacy"?
+            <div className="w-dvw h-dvh flex justify-center items-center bg-black bg-opacity-60 absolute top-0 left-0">
+                <div className="flex flex-col bg-white rounded-2xl border border-main-black w-80 h-96 overflow-scroll p-4">
+                    <button className="ml-auto mr-0 hover:text-main-green-500" onClick={()=>setFormToDisplay("")}>
+                        <FontAwesomeIcon icon={faX}/>
+                    </button>
+                    {data.terms.privacy_policy.map((info,index)=>(
+                        <>
+                            <h3 key={index + "title"} className={`font-bold w-full ${index===0? "text-center underline mb-2 text-lg":"mb-2"}`}>
+                                { info.title}
+                            </h3>
+                            <p key={index + "body"} className="mb-6">
+                                {info.body}
+                            </p>
+                        </>
+                    ))}
+                </div>
+            </div>
+            :
+            <div className="mb-5 text-center">
+                <input
+                    type={data.terms.inputs[0].type}
+                    name="agreement"
+                    onClick={()=>setClicked(!clicked)}
+                    required
+                />
+                <label htmlFor="agreement" className="ml-4 text-center">
+                    I have read and agree to the 
+                    <span className="underline cursor-pointer text-main-green-700 ml-2 hover:text-main-green-500" onClick={()=>viewAgreement("terms")}>
+                        Terms and Conditions
+                    </span>
+                    , and 
+                    <span className="underline cursor-pointer text-main-green-700 ml-2 hover:text-main-green-500" onClick={()=>viewAgreement("privacy")}>
+                        Privacy Policy.
+                    </span> 
+                </label>
+            </div>
+            )  
+        }
+    </div>
     )
 }
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -77,7 +77,7 @@ export default function SignUp(){
     }, [])
 
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
+        e.preventDefault()
         if (state.currentSeries < signupQuestions.length - 1) {
           dispatch({ type: "NEXT_SERIES" })
         } else {
@@ -89,85 +89,86 @@ export default function SignUp(){
 
     return (
         <div className="flex flex-col h-dvh p-2">
-        <h2 className="heading mt-20">{signupQuestions[state.currentSeries].title}</h2>
-        <div className="flex flex-col justify-center items-center py-12">
-            <form className="flex flex-col justify-center w-full mx-auto px-11 md:w-7/12 lg:w-4/12" onSubmit={handleSubmit}>
-                {currentInputs.map((input) => (
-                    <div className="relative w-full mb-5" key={input.id}>
-                    {input.type !== "select" ? (
-                    <>
-                        <input
-                            id={input.id}
-                            name={input.name}
-                            type={input.type}
-                            placeholder={input.placeholder}
-                            className={`form-input ${input.type==='date'? 'date-input': '' }`}
-                            required
-                        />
-                        {input.type!=='date'?
-                            <FontAwesomeIcon
-                                icon={iconMap[input.icon]}
-                                className="absolute inset-y-4 left-0 flex items-center pl-4 text-main-grey-500"
-                            />
+          <h2 className="heading mt-20">{signupQuestions[state.currentSeries].title}</h2>
+          <div className="flex flex-col justify-center items-center py-12">
+              <form className="flex flex-col justify-center w-full mx-auto px-11 md:w-7/12 lg:w-4/12" onSubmit={handleSubmit}>
+                  {currentInputs.map((input) => (
+                      <div className="relative w-full mb-5" key={input.id}>
+                      {input.type !== "select" ? (
+                      <>
+                          <input
+                              id={input.id}
+                              name={input.name}
+                              type={input.type}
+                              placeholder={input.placeholder}
+                              className={`form-input ${input.type==='date'? 'date-input': '' }`}
+                              required
+                              autoComplete="on"
+                          />
+                          {input.type!=='date'?
+                              <FontAwesomeIcon
+                                  icon={iconMap[input.icon]}
+                                  className="absolute inset-y-4 left-0 flex items-center pl-4 text-main-grey-500"
+                              />
+                          :
+                              <></>
+                          }
+                      </>
+                      ) : (
+                          <>
+                          <select id={input.id} name={input.name} className="form-input appearance-none" required>
+                          {input.options?.map((option, index) => (
+                              index === 0 ? (
+                                  <option key={option.value} disabled selected={option.selected} value="">
+                                  {option.placeholder}
+                                  </option>
+                              ) : (
+                                  <option key={option.value} value={option.value} selected={option.selected}>
+                                      {option.placeholder}
+                                  </option>
+                              )
+                          ))}
+                          
+                          </select>
+                          <FontAwesomeIcon
+                              icon={iconMap[input.icon]}
+                              className="absolute inset-y-4 left-0 flex items-center pl-4 text-main-grey-500"
+                          />
+                      </>
+                      )}
+                      </div>
+                  ))}
+                  {state.currentSeries === signupQuestions.length - 1 ?
+                        <AgreementTerms isClicked = {agreeToTerms} /> 
                         :
-                            <></>
-                        }
-                    </>
-                    ) : (
-                        <>
-                        <select id={input.id} name={input.name} className="form-input appearance-none" required>
-                        {input.options?.map((option, index) => (
-                            index === 0 ? (
-                                <option key={option.value} disabled selected={option.selected} value="">
-                                {option.placeholder}
-                                </option>
-                            ) : (
-                                <option key={option.value} value={option.value} selected={option.selected}>
-                                    {option.placeholder}
-                                </option>
-                            )
-                        ))}
-                        
-                        </select>
-                        <FontAwesomeIcon
-                            icon={iconMap[input.icon]}
-                            className="absolute inset-y-4 left-0 flex items-center pl-4 text-main-grey-500"
-                        />
-                    
-                    </>
-                    )}
-                    </div>
-                ))}
-                {state.currentSeries === signupQuestions.length - 1 ?
-                      <AgreementTerms isClicked = {agreeToTerms} /> 
-                      :
-                      <></>
-                }
-                <button
-                    className="w-full form-btn mx-auto"
-                    aria-label="sign up form submission"
-                    type="submit"
-                >
-                    {signupQuestions[state.currentSeries].button}
-                </button>
-                </form>
-                <div className="flex justify-center items-center my-7">
-                    <div className="bg-main-grey-300 h-0.5 w-28 lg:w-40"></div>
-                    <p className="mx-3 text-main-grey-200">Or Sign Up With</p>
-                    <div className="bg-main-grey-300 h-0.5 w-28 lg:w-40"></div>
-                </div>
-                <div className="mb-9">
-                    <SignInPartners />
-                </div>
-                <p className="text-main-grey-300 font-kulim">
-                    Already have an account? 
-                    <Link to={'/login'}>
-                        <span className="underline cursor-pointer text-main-green-700 ml-1 hover:text-main-green-500">
-                            Login Here
-                        </span>
-                    </Link>
-                </p>
-            </div>
+                        <></>
+                  }
+                  <button
+                      className="w-full form-btn mx-auto"
+                      aria-label="sign up form submission"
+                      type="submit"
+                      disabled={agreeToTerms=== true && state.currentSeries === signupQuestions.length - 1}
+                  >
+                      {signupQuestions[state.currentSeries].button}
+                  </button>
+              </form>
+              <div className="flex justify-center items-center my-7">
+                  <div className="bg-main-grey-300 h-0.5 w-28 lg:w-40"></div>
+                  <p className="mx-3 text-main-grey-200">Or Sign Up With</p>
+                  <div className="bg-main-grey-300 h-0.5 w-28 lg:w-40"></div>
+              </div>
+              <div className="mb-9">
+                  <SignInPartners />
+              </div>
+              <p className="text-main-grey-300 font-kulim">
+                  Already have an account? 
+                  <Link to={'/login'}>
+                      <span className="underline cursor-pointer text-main-green-700 ml-1 hover:text-main-green-500">
+                          Login Here
+                      </span>
+                  </Link>
+              </p>
+          </div>
         </div>
 
     )
