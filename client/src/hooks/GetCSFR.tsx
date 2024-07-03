77e49b29a2b06fd01f240e9a3c85dfb27ee8d1fd
mikegag <Michael.gagliardi1@hotmail.com>
2024-07-03 19:38:12 -0400

    created api proxy for frontend and GetCSFR hook

---
+++ client/src/hooks/GetCSFR.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+interface CsfrProps {
+    name: string;
+}
+
+function getCookie(name: string): string | null {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+export function useCSFR({ name }: CsfrProps): string | null {
+    const [csrfToken, setCsrfToken] = useState<string | null>(null);
+
+    useEffect(() => {
+        const token = getCookie(name);
+        setCsrfToken(token);
+    }, [name]);
+
+    return csrfToken;
+}
