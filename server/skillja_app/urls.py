ce0adcf249725b54888ebac74211eea2134b856a
mikegag <Michael.gagliardi1@hotmail.com>
2024-07-03 20:25:01 -0400

    updated urls

--- server/skillja_app/urls.py
+++ server/skillja_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('csrf_token/', csrf_token, name='csrf_token'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('signup/', views.sign_up, name='signup'),
