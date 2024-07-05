ec021e5c0e0731583db896a0aecae6f7475723be
mikegag <Michael.gagliardi1@hotmail.com>
2024-07-05 11:44:28 -0400

    updated settings

--- server/skillja_project/settings.py
+++ server/skillja_project/settings.py
@@ -100,9 +100,9 @@ WSGI_APPLICATION = "skillja_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-     "default": dj_database_url.config(default=DATABASE_URL)
-}
+# DATABASES = {
+#      "default": dj_database_url.config(default=DATABASE_URL)
+# }
 
 
 # Password validation
