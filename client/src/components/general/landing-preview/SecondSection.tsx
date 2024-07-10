bed651af220a6f72b6c0d869753a238eb5e18a0a
mikegag <Michael.gagliardi1@hotmail.com>
2024-07-10 11:34:59 -0400

    updated SecondSection component

--- client/src/components/general/landing-preview/SecondSection.tsx
+++ client/src/components/general/landing-preview/SecondSection.tsx
@@ -2,12 +2,15 @@ import React from "react";
 
 export default function SecondSection(){
     return (
-        <div className="bg-main-cream flex justify-center items-center px-2">
+        <div className="bg-main-cream flex flex-wrap justify-center items-center px-2">
             <img 
                 src={require('../../../assets/landingAssets/woman-jogging.png')}
-                className="w-80 mt-6 mb-5"
+                className="w-80 mr-5"
                 alt="woman jogging outside"
             />
+            <div>
+
+            </div>
         </div>
     )
 }
