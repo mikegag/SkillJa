69695414171a4660eb35ab8d4b5d3aca81a9dd29
mikegag <Michael.gagliardi1@hotmail.com>
2024-07-16 16:11:59 -0400

    testing proxy url

--- client/src/hooks/CreateCSFR.tsx
+++ client/src/hooks/CreateCSFR.tsx
@@ -7,13 +7,11 @@ interface CsfrProps {
 
 export default function CreateCSFR({ name }: CsfrProps): string | null {
     const [csrfToken, setCsrfToken] = useState<string | null>(null)
-    const api = process.env.REACT_APP_RAILWAY_URL 
 
     useEffect(() => {
         const fetchCsrfToken = async () => {
-            console.log(api)
             try {
-                const res = await axios.get(`/${api}/csrf_token/`)
+                const res = await axios.get('/api/csrf_token/')
                 const token = res.data.csrfToken
                 if (token) {
                     setCsrfToken(token)
