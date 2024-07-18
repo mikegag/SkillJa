9418b9dfe6d1e50f4e33a8ee636e1b470bbc4d87
mikegag <Michael.gagliardi1@hotmail.com>
2024-07-18 17:58:32 -0400

    updated settings

--- server/skillja_project/settings.py
+++ server/skillja_project/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'client' / 'build'
+    BASE_DIR / '../../client' / 'build'
 ]
 
 
