81dfc09497f3ef86d09fc7d23d6fd1d28bd081a1
mikegag <Michael.gagliardi1@hotmail.com>
2024-07-19 15:28:44 -0400

    updated settings

--- server/skillja_project/settings.py
+++ server/skillja_project/settings.py
@@ -143,11 +143,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+BASE_DI = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR) 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'client' / 'build'
+    BASE_DIR / 'client' / 'build' / 'static'
 ]
 
 
