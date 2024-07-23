33d125100180f21241f98d99ab04bce5062217f7
mikegag <Michael.gagliardi1@hotmail.com>
2024-07-22 21:09:50 -0400

    updated urls

--- server/skillja_app/urls.py
+++ server/skillja_app/urls.py
@@ -4,8 +4,8 @@ from .views import csrf_token
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', views.index, name='index'),
+    # path('', TemplateView.as_view(template_name='index.html')),
     # path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json'), name='manifest.json'),
     path('csrf_token/', csrf_token, name='csrf_token'),
     path('login/', views.user_login, name='login'),
