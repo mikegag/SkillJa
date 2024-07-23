cec16fe794f7040d133bcfbbe86f783acede074d
mikegag <Michael.gagliardi1@hotmail.com>
2024-07-23 15:59:56 -0400

    updated path for login request

--- server/skillja_app/urls.py
+++ server/skillja_app/urls.py
@@ -5,20 +5,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('', TemplateView.as_view(template_name='index.html')),
-    # path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json'), name='manifest.json'),
     path('csrf_token/', csrf_token, name='csrf_token'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('signup/', views.sign_up, name='signup'),
-    path('auth/onboarding/', views.onboarding_user, name='onboarding'),
-    # path('auth/home-feed/', views.logout_view, name='home-feed'),
-    # path('auth/coach/<int:pk>/', views.logout_view, name='home-feed'),
-    # path('auth/calendar/', views.logout_view, name='calendar'),
-    # path('auth/profile/', views.logout_view, name='profile'),
-    # path('auth/profile/settings/account-information/', views.logout_view, name='account-information'),
-    # path('auth/profile/settings/payments/', views.logout_view, name='payments'),
-    # path('auth/profile/settings/notifications/', views.logout_view, name='notifications'),
-    # path('auth/profile/settings/faqs/', views.logout_view, name='faqs'),
-    # path('auth/profile/settings/report-issue/', views.logout_view, name='report-issue')
+    path('auth/onboarding/', views.onboarding_user, name='onboarding')
 ]   
