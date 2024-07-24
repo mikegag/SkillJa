ac680f19294744ce838eb3307ff9acd4476bbd50
mikegag <Michael.gagliardi1@hotmail.com>
2024-07-23 21:11:26 -0400

    updated settings

--- server/skillja_project/urls.py
+++ server/skillja_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from skillja_app import views
 
 
 urlpatterns = [
