325570cbed92f8e847ec48ead3390c7bf5c8987c
mikegag <Michael.gagliardi1@hotmail.com>
2024-09-06 19:37:46 -0400

    updated Service model in backend

--- server/skillja_app/models.py
+++ server/skillja_app/models.py
@@ -130,9 +130,9 @@ class AthleteProfile(models.Model):
 
 class Service(models.Model):
     TYPE_CHOICES = (
-        ('full-program':'full-program'),
-        ('online-program': 'online-program'),
-        ('individual-session':'individual-session')
+        ('full-program','full-program'),
+        ('online-program', 'online-program'),
+        ('individual-session','individual-session')
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='services')
     type = models.CharField(max_length=20, choices = TYPE_CHOICES, default='individual-session')
