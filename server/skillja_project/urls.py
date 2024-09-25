cb9928b4f5555a2d7bdf100511a921c6b7832f6e
mikegag <Michael.gagliardi1@hotmail.com>
2024-09-25 12:33:02 -0400

    updated urls to handle page refresh

--- server/skillja_project/urls.py
+++ server/skillja_project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 from skillja_app import views
@@ -24,4 +25,6 @@ from skillja_app import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('skillja_app.urls')), 
+    # Catch-all URL pattern to serve the React app
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
