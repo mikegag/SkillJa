9065b91f219c8ba53e359da9ab8403d995f04050
mikegag <Michael.gagliardi1@hotmail.com>
2024-09-25 11:54:10 -0400

    minor changes made to ComingSoon page

--- client/src/pages/ComingSoon.tsx
+++ client/src/pages/ComingSoon.tsx
@@ -1,18 +1,18 @@
-import { faLongArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import { faLongArrowLeft } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function ComingSoon(){
     const navigate = useNavigate()
     return (
-        <div className="flex justify-center items-center mt-28">
+        <div className="flex justify-center items-center mt-28 px-8">
             <FontAwesomeIcon 
-                    className="text-4xl my-auto ml-auto mr-5 hover:text-main-green-500 cursor-pointer"
+                    className="text-2xl lg:text-4xl my-auto ml-auto mr-2 lg:mr-5 hover:text-main-green-500 cursor-pointer"
                     icon={faLongArrowLeft} 
                     onClick={(()=>navigate('../'))}
             />
-            <p className="font-kulim text-2xl text-center mr-auto">
+            <p className="font-kulim text-xl w-fit lg:text-2xl text-center mr-auto">
                 This page is still under construction 🛠️
             </p>
         </div>
