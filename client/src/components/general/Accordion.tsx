de8eec10dabac24310aacf21acb8608a2cd9854f
mikegag <michael.gagliardi1@hotmail.com>
2024-11-22 17:49:57 -0500

    minor styling change made to Accordion component

--- client/src/components/general/Accordion.tsx
+++ client/src/components/general/Accordion.tsx
@@ -17,14 +17,14 @@ export default function Accordion({ title, children, style }: AccordionProps) {
     setAnswerHeight(isOpen ? answerRef.current?.scrollHeight : 0)
   }, [isOpen])
 
-  const toggleAccordion = () => {
+  function toggleAccordion(){
     setIsOpen(!isOpen)
   }
 
   return (
     <div className={`rounded-2xl mb-5 mx-auto border bg-white w-72 font-source overflow-hidden ${style? style :'border-main-grey-200'}`}>
       <button
-        onClick={toggleAccordion}
+        onClick={(e)=>{toggleAccordion; e.preventDefault();}}
         className={`flex justify-between items-center w-full p-3   ${isOpen ? 'rounded-b-none' : ''}`}
       >
         <p className="bg-main-white font-source font-medium text-main-black text-base">{title}</p>
