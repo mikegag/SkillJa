ae8f2b7a061c6cbbb17e0970d868da3b2313c37e
mikegag <michael.gagliardi1@hotmail.com>
2024-11-30 12:28:27 -0500

    removed unused proxy, created fallback error page and view for 500 server errors

--- server/skillja_app/views.py
+++ server/skillja_app/views.py
@@ -569,6 +569,9 @@ def auth_status(request):
             'username': None
         })
 
+def custom_500(request):
+    return render(request, '500.html', status=500)
+
 @api_view(['GET'])
 def getRoutes(request):
 
